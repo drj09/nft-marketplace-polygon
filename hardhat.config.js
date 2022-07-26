164f6a1219b2dfa4d4e8eb5dba1162a1b412b6cc
Dheeraj <Dheeraj160199@gmail.com>
2022-07-26 13:12:41 +0530

    created NFTMarket Smart Contract

---
+++ hardhat.config.js
@@ -0,0 +1,18 @@
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config();
+
+console.log(process.env.POLYGON_MUMBAI_URL);
+
+const POLYGON_MUMBAI_URL = process.env.POLYGON_MUMBAI_URL;
+const METAMASK_PRIVATE_KEY = process.env.METAMASK_PRIVATE_KEY;
+/** @type import('hardhat/config').HardhatUserConfig */
+module.exports = {
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    mumbai: { url: POLYGON_MUMBAI_URL, accounts: [METAMASK_PRIVATE_KEY] },
+    mainnet: { url: "", accounts: [METAMASK_PRIVATE_KEY] },
+  },
+  solidity: "0.8.9",
+};
