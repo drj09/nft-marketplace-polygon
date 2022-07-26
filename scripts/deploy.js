17c115001dbef28e7a83eb3ac3dfefc85ca09485
Dheeraj <Dheeraj160199@gmail.com>
2022-07-26 21:40:48 +0530

    added create-nft.js

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,22 +1,25 @@
 const hre = require("hardhat");
+const fs = require("fs");
 
 async function main() {
-    const NFTMarket = await hre.ethers.getContractFactory("NFTMarketplace");
-    const nftMarket = await NFTMarket.deploy();
-
-    await nftMarket.deployed();
-
-    console.log("Lock with 1 ETH deployed to:", nftMarket.address);
+    const NFTMarketplace = await hre.ethers.getContractFactory(
+        "NFTMarketplace"
+    );
+    const nftMarketplace = await NFTMarketplace.deploy();
+    await nftMarketplace.deployed();
+    console.log("nftMarketplace deployed to:", nftMarketplace.address);
 
     fs.writeFileSync(
         "./config.js",
-        `export const nftMarketAddress = "${nftMarketplace.address}"`
+        `
+  export const nftMarketAddress = "${nftMarketplace.address}"
+  `
     );
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-    console.error(error);
-    process.exitCode = 1;
-});
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error);
+        process.exit(1);
+    });
